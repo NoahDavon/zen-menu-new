9cf986ea83f9e3ca8290054d91f19fb879990c29
Mohammed <MohammedE.Fahmy@outlook.com>
2023-09-10 21:34:57 +0300

    First Commit

---
+++ zen-menu-new/app/components/productCard.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { FunctionComponent } from 'react';
+import { Image } from '@chakra-ui/react';
+interface Props {
+    menu: boolean;
+}
+ 
+const ProductCard: FunctionComponent<Props> = ({menu = true}) => {
+    return ( <div></div> );
+}
+ 
+export default ProductCard;
