560f071e58cc705b780f73b0ea66d2ec8782e16a
Mohammed <MohammedE.Fahmy@outlook.com>
2023-09-10 21:35:13 +0300

    Product Card

--- zen-menu-new/app/components/productCard.tsx
+++ zen-menu-new/app/components/productCard.tsx
@@ -2,11 +2,11 @@ import * as React from 'react';
 import { FunctionComponent } from 'react';
 import { Image } from '@chakra-ui/react';
 interface Props {
-    menu: boolean;
+    menu?: boolean;
 }
  
 const ProductCard: FunctionComponent<Props> = ({menu = true}) => {
-    return ( <div></div> );
+    return ( <Image src='/ProductImages/Cappuccino_at_Sightglass_Coffee.jpg' maxWidth={"40vw"} rounded="80"/> );
 }
  
 export default ProductCard;
