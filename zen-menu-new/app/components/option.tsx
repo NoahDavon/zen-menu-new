a226f49a6e1bd06563436a930310166631159447
Mohammed <MohammedE.Fahmy@outlook.com>
2023-09-17 19:52:46 +0300

    Options

--- zen-menu-new/app/components/option.tsx
+++ zen-menu-new/app/components/option.tsx
@@ -1,14 +1,20 @@
 import { Flex, Heading } from '@chakra-ui/react'
 import React, { useState } from 'react'
 
-type Props = {}
+type Props = {
+    options: Number[]
+}
 
-export default function Option({}: Props) {
-    const [options, setOptions] = useState(null);
+export default function Option({options}: Props) {
+    const [selected, setSelected] = useState(null);
   return (
     <>
     <Heading size='md'>Option Name</Heading>
-    <Flex className=' flex-wrap '></Flex>
+    <Flex className=' flex-wrap '>
+        {options.map((x)=>{return(
+            <div></div>
+        )})}
+    </Flex>
     </>
   )
 }
