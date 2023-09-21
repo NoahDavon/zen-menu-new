198495ab0c54945ca7bffb3c4bf7a91f44f4cb57
Mohammed <MohammedE.Fahmy@outlook.com>
2023-09-22 00:20:51 +0300

    Options

--- zen-menu-new/app/components/option.tsx
+++ zen-menu-new/app/components/option.tsx
@@ -3,21 +3,22 @@ import React, { useState } from 'react'
 import SelectButton from './selectButton';
 
 type Props = {
-    options: string[];
+    Name: string,
+    options: {[key: string]: number};
     isOption?: Boolean;
 }
 
 
-export default function Option({options, isOption=false}: Props) {
-    const [selected, setSelected] = useState<{[key: string]: Boolean}>(!isOption? {} : {[options[0]]: true});
+export default function Option({Name, options, isOption=false}: Props) {
+    const [selected, setSelected] = useState<{[key: string]: Boolean}>(!isOption? {} : {[Object.keys(options)[0]]: true});
     const handleClick = (x: string) => {
         isOption? setSelected({[x]:(true)}):setSelected({...selected, [x]: (!selected[x]??true)})
     }
   return (
     <>
-    <Heading size='sm'>Option Name</Heading>
+    <Heading size='sm'>{Name}</Heading>
     <Flex className=' flex-wrap justify-between items-center '>
-        {options.map((x)=>{return(
+        {Object.keys(options).map((x)=>{return(
             <SelectButton onClick={()=> {handleClick(x)}} highlighted={(selected[x]??false)} name={x}/>
         )})}
     </Flex>
--- zen-menu-new/app/components/productDetails.tsx
+++ zen-menu-new/app/components/productDetails.tsx
@@ -1,10 +1,11 @@
 'use client'
 import { AddIcon, MinusIcon } from '@chakra-ui/icons';
 import { Button, Image, Modal, ModalCloseButton, ModalContent,ModalBody, ModalHeader, ModalOverlay, Text, Heading, Spacer, Flex, IconButton, Stack } from '@chakra-ui/react'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Option from './option';
 import AdditionalNotes from './AdditionalNotes';
 import { Item } from './menu';
+import { getAdditions } from '../firebase';
 export interface Option{
   Name: string,
   Options: {[key:string]: number},
@@ -17,6 +18,21 @@ type Props = {
   Item?: Item
 }
 export default function ProductDetails({onOpen, onClose, isOpen, Item}: Props) {
+  function Counter({}) {
+    return (
+      <div className='w-full h-12 flex bg-white border border-orange-800 rounded-xl items-center shadow'>
+          <Heading size='md' className='font-bold p-4 text-[#BC5F00]'>{Item?.Name}</Heading>
+          <Spacer/>
+          <Flex className='w-1/3 items-center mx-4'>
+              <IconButton icon={<MinusIcon/>} aria-label='remove' className='rounded-full text-[#BC5F00]'/>
+              <Text className='px-4'>3</Text>
+              <IconButton icon={<AddIcon/>} aria-label='add' className='rounded-full text-[#BC5F00]'/>
+          </Flex>
+      </div>
+    )
+  }
+  const [additions, setAdditions] = useState<Option[]>([]);
+  useEffect(()=>{getAdditions(Item as Item).then((adds)=> setAdditions(adds))},[])
   return (
     <>
     <Modal size='full' isOpen={isOpen} onClose={onClose}>
@@ -31,9 +47,10 @@ export default function ProductDetails({onOpen, onClose, isOpen, Item}: Props) {
               <Stack>
                 <Counter/>
                 <Text className='text-center'>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero tenetur facilis placeat, incidunt distinctio sint minus corrupti nemo architecto cupiditate eligendi doloribus. Aperiam veritatis laborum minus nec
+                  {Item?.description}
                 </Text>
-                <Option isOption options={["milk", "no milk"]}/>
+                {additions.map((addition)=>
+                <Option Name={addition.Name} isOption={addition.isOption} options={addition.Options}/>)}
                 <AdditionalNotes/>
                 <Button variant='outline'>Add To Cart</Button>
               </Stack>
@@ -41,21 +58,5 @@ export default function ProductDetails({onOpen, onClose, isOpen, Item}: Props) {
         </ModalContent>
     </Modal>
     </>
-  )
-}
-
-type CounterProps = {}
-
-function Counter({}: CounterProps) {
-  return (
-    <div className='w-full h-12 flex bg-white border border-orange-800 rounded-xl items-center shadow'>
-        <Heading size='md' className='font-bold p-4 text-[#BC5F00]'>Cappucino</Heading>
-        <Spacer/>
-        <Flex className='w-1/3 items-center mx-4'>
-            <IconButton icon={<MinusIcon/>} aria-label='remove' className='rounded-full text-[#BC5F00]'/>
-            <Text className='px-4'>3</Text>
-            <IconButton icon={<AddIcon/>} aria-label='add' className='rounded-full text-[#BC5F00]'/>
-        </Flex>
-    </div>
   )
 }
