7b84c8fe1f93a30825f86ce928917dd83791ef66
Mohammed <MohammedE.Fahmy@outlook.com>
2023-09-28 17:50:14 +0300

    Done x4

--- zen-menu-new/app/components/productCard.tsx
+++ zen-menu-new/app/components/productCard.tsx
@@ -13,18 +13,18 @@ interface Props {
 const ProductCard: FunctionComponent<Props> = ({menu = false, item}) => {
     const {onOpen, onClose, isOpen} = useDisclosure();
     return (<> 
-        {!menu?<Card onClick={onOpen} className=" mx-4 my-4 rounded-2xl justify-end flex-shrink-0 w-52 h-80 bg-cover bg-center" backgroundImage={`/ProductImages/${item.Name}.jpg`}>
+        {!menu?<Card onClick={onOpen} className=" mx-4 my-4 rounded-2xl justify-end flex-shrink-0 w-60 h-80 bg-cover bg-center" backgroundImage={`/ProductImages/${item.Name}.jpg`}>
             <Card className="text-black bg-white font-sans items-center rounded-2xl m-2 p-2">
                 <Flex color={"#BC5F00"} className="pt-2 text-base font-normal w-[100%]">
                         <Text fontSize='smaller'>{item.Name}</Text>
                         <Spacer/>
-                        <Text fontSize='smaller' textDecor='line-through' color='gray.500'>{item.Previous}</Text>
+                        <Text fontSize='smaller' textDecor='line-through' color='gray.500' className='mx-1'>{item.Previous}</Text>
                         <Text fontSize='smaller'>{item.Price} LE</Text>
                 </Flex>
                 <Text noOfLines={2} size='sm' className='text-xs mx-2 text-left'>{item.description??''}</Text>           
             </Card>
         </Card> :
-        <Card onClick={onOpen} flexDirection='row' className='rounded-2xl aspect-[4/1] w-full my-3 max-h-32'>
+        <Card onClick={onOpen} flexDirection='row' className='rounded-2xl aspect-[4/1] w-full my-3 h-28'>
             <Image src={`/ProductImages/${item.Name}.jpg`} className='w-3/12 object-cover rounded-md shrink-0'/>
             <Stack spacing='0.1rem' className='p-2'>
                 <Text color={"#BC5F00"} className="text-sm font-normal font-sans ">{item.Name}</Text>
